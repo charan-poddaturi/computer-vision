d2b526c812abc0fe3eb0856d43051a251e33f74b
charan-poddaturi <24r11a6735@gcet.edu.in>
2026-06-28 17:40:56 +0530

    ..

---
+++ assign1.py
@@ -0,0 +1,20 @@
+import cv2
+image = cv2.imread('trial.jpeg')
+if image is None:
+    print("Error: Could not read the image.")
+else:
+    h, w, c = image.shape
+    print(f"Image dimensions: {w}x{h}, Channels: {c}")
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    if gray is not None:
+        print("image converted to grayscale, do you want to see?")
+        uin = input("Enter 'yes' or 'no': ")
+        if uin.lower() == "yes":
+            cv2.imshow('Gray Image', gray)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            print("Saving the grayscale image as 'trial_gray.jpeg'...")
+        else:
+            print("Okay, not displaying the image.")
+            print("Saving the grayscale image as 'trial_gray.jpeg'...")
+cv2.imwrite('trial_gray1.jpeg', gray)
